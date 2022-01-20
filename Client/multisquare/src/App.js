652250d7baccccfb2d8883f52ff384c4cf30d306
itsaditya01 <@Ditya001>
2022-01-20 17:26:58 +0530

    Enhancing Ecommerce.js

--- Client/multisquare/src/App.js
+++ Client/multisquare/src/App.js
@@ -7,22 +7,30 @@ import { Switch, Route } from "react-router-dom";
 import Login from "./components/Authentication/Login";
 import "./App.css";
 import SignUp from "./components/Authentication/Signup";
+import Ecommerce from "./components/backend/Ecommerce";
 
 const App = () => {
   return (
     <>
       <Router>
         <AuthProvider>
-          <Navbar />
           <Switch>
-            <Route exact path="/">
-              <Products />
+            <Route exact path="/backend">
+              <Ecommerce />
             </Route>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-            <Route exact path="/signup">
-              <SignUp />
+            <Route>
+              <Navbar />
+              <Switch>
+                <Route exact path="/">
+                  <Products />
+                </Route>
+                <Route exact path="/login">
+                  <Login />
+                </Route>
+                <Route exact path="/signup">
+                  <SignUp />
+                </Route>
+              </Switch>
             </Route>
           </Switch>
         </AuthProvider>
